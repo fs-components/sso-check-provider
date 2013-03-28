de095b614f9e6e717851459c3f8dc49a5aaeabb4
Cameron Bytheway <cameron@nujii.com>
2013-03-28 17:05:10 -0600

    Allow custom logged in checking

--- index.js
+++ index.js
@@ -4,15 +4,20 @@
 var xdm = require("easyXDM")
   , cookie = require("cookie");
 
-module.exports = function(name, options) {
+module.exports = function(cb, options) {
 
   if(!options) options = {};
 
+  if (typeof cb === "string") {
+    var name = cb;
+    cb = function(success, error) {
+      return typeof cookie(name) !== "undefined";
+    };
+  };
+
   var rpc = new xdm.Rpc(options, {
     local: {
-      loggedIn: function(success, error) {
-        return typeof cookie(name) !== "undefined";
-      }
+      loggedIn: cb
     }
   });
 
